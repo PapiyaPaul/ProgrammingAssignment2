445a8d2202a3d348798c499d371a17fc1962de28
PapiyaPaul <39119460+PapiyaPaul@users.noreply.github.com>
2018-08-06 04:32:04 -0700

    Assignment program for Papiya Paul
    Committed on 6th August 5 PM IST

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#R Assignement for Papiya Paul (papiya.paul@tcs.com)
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function creates a special "matrix" object that can cache its inverse 
 
+makeCacheMatrix <- function(x=matrix()) {
+
+    inv_matrix = NULL  # stores the inverse matrix
+
+    set <- function(y) {  # setter method of input matrix
+	x <<-y
+	inv_matrix <<- NUL
+    }
+
+    get <- function() x  # getter method of input matrix
+
+    setInverse <- function(inverse) inv_matrix <<- inverse  # setter method of inverse matrix
+
+    getInverse <- function() inv_matrix  # getter method of inverse matrix
+
+    list(set = set,
+	 get = get,
+	 setInverse = setInverse,
+	 getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+#  This function take a matrix as input
+#  check into the cache if it is already calculated
+#  if not it generate the inverse matrix
+#  return inverse matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x,...){
+
+   inv_matrix <- x$getInverse() # get the matrix inverse
+
+   if(!is.null(inv_matrix)) { # check if inverse matrix already present in cache
+      message("inverse matrix fetched from cache")
+      return(inv_matrix)
+   }
+
+   # if inverse matrix is not present in cache
+   matrix <- x$get()  # get input matrix 
+   message("inverse matrix getting calculated")
+   inv_matrix <- solve(matrix,...)   # calculate inverse matrix
+   x$setInverse(inv_matrix) # set inverse matrix
+   inv_matrix  # return inverse matrix
+
+} 
